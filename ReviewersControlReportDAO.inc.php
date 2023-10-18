ed5263c45a816c9eebcf6fe758452f28d460ac23
yves <yves@lepidus.com.br>
2023-10-18 11:40:19 -0400

    Add "leftJoin" SQL clause to retrieve givenName.
    documentacao-e-tarefas/desenvolvimento_e_infra#640
    
    Signed-off-by: Iudi <iudi@lepidus.com.br>
    Signed-off-by: Yves <yves@lepidus.com.br>

--- ReviewersControlReportDAO.inc.php
+++ ReviewersControlReportDAO.inc.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Collection;
 
 class ReviewersControlReportDAO extends DAO
 {
+    private $locale;
+
     public function getReviewers($journalId)
     {
         $resultGroup = Capsule::table('user_groups')
@@ -16,12 +18,17 @@ class ReviewersControlReportDAO extends DAO
         ->first();
         $userGroupId = get_object_vars($resultGroup)['user_group_id'];
 
+        $this->locale = AppLocale::getLocale();
+
         $query = Capsule::table('users AS u')
-        ->select('u.email')
+        ->select('u.email', 'us.setting_value AS givenName')
         ->whereIn('u.user_id', function ($query) use ($userGroupId) {
             $query->select('user_id')
             ->from('user_user_groups')
             ->where('user_group_id', $userGroupId);
+        })->leftJoin('user_settings AS us', 'us.user_id', '=', 'u.user_id')->where(function ($query) use ($givenName) {
+            $query->where('us.setting_name', '=', 'givenName')
+            ->where('us.locale', '=', $this->locale);
         });
 
         return $query->get();
