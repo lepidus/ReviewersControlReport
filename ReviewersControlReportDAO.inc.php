c9aa4f7d6b93c534c1c8439cdbe6cb93f54dcd30
yves <yves@lepidus.com.br>
2023-10-18 14:40:29 -0400

    Add "leftJoin" SQL clause to retrieve familyName.
    documentacao-e-tarefas/desenvolvimento_e_infra#640
    
    Signed-off-by: Iudi <iudi@lepidus.com.br>
    Signed-off-by: Yves <yves@lepidus.com.br>

--- ReviewersControlReportDAO.inc.php
+++ ReviewersControlReportDAO.inc.php
@@ -21,14 +21,17 @@ class ReviewersControlReportDAO extends DAO
         $this->locale = AppLocale::getLocale();
 
         $query = Capsule::table('users AS u')
-        ->select('u.email', 'us.setting_value AS givenName')
+        ->select('u.email', 'us_givenName.setting_value AS givenName', 'us_familyName.setting_value as familyName')
         ->whereIn('u.user_id', function ($query) use ($userGroupId) {
             $query->select('user_id')
             ->from('user_user_groups')
             ->where('user_group_id', $userGroupId);
-        })->leftJoin('user_settings AS us', 'us.user_id', '=', 'u.user_id')->where(function ($query) use ($givenName) {
-            $query->where('us.setting_name', '=', 'givenName')
-            ->where('us.locale', '=', $this->locale);
+        })->leftJoin('user_settings AS us_givenName', 'us_givenName.user_id', '=', 'u.user_id')->where(function ($query) use ($givenName) {
+            $query->where('us_givenName.setting_name', '=', 'givenName')
+            ->where('us_givenName.locale', '=', $this->locale);
+        })->leftJoin('user_settings AS us_familyName', 'us_familyName.user_id', '=', 'u.user_id')->where(function ($query) use ($familyName) {
+            $query->where('us_familyName.setting_name', '=', 'familyName')
+            ->where('us_familyName.locale', '=', $this->locale);
         });
 
         return $query->get();
