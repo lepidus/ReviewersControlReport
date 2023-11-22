119052182be0f1bd97b3ef1249eed7f9ebb897e8
yves <yves@lepidus.com.br>
2023-11-22 14:05:20 -0400

    Add utf8 validation.
    documentacao-e-tarefas/desenvolvimento_e_infra#640
    
    Signed-off-by: Iudi <iudi@lepidus.com.br>
    Signed-off-by: Yves <yves@lepidus.com.br>
    Signed-off-by: Thiago <thiago@lepidus.com.br>

--- classes/traits/StringLength.inc.php
+++ classes/traits/StringLength.inc.php
@@ -4,10 +4,14 @@ trait StringLength
 {
     public function formatStringLength(string $string, int $length = 30): string
     {
-        if (strlen($string) > $length) {
-            $firstStringIndex = 0;
-            $threeCharactersFromTheEnd = $length - 3;
-            return substr($string, $firstStringIndex, $threeCharactersFromTheEnd) . '...';
+        if (mb_check_encoding($string, 'UTF-8')) {
+            if (mb_strlen($string, 'UTF-8') > $length) {
+                $firstStringIndex = 0;
+                $threeCharactersFromTheEnd = $length - 3;
+                return mb_substr($string, $firstStringIndex, $threeCharactersFromTheEnd, 'UTF-8') . '...';
+            }
+        } else {
+            return __('plugins.reports.reviewersControlReport.validate.utf8');
         }
 
         return $string;
