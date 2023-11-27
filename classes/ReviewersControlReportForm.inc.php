f17b9010e123db2616bdfd9fcb6de5f454751214
yves <yves@lepidus.com.br>
2023-11-27 12:41:56 -0400

    Add more fields to CSV.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#681
    
    Signed-off-by: Yves <yves@lepidus.com.br>
    Signed-off-by: Thiago <thiago@lepidus.com.br>

--- classes/ReviewersControlReportForm.inc.php
+++ classes/ReviewersControlReportForm.inc.php
@@ -12,10 +12,17 @@ class ReviewersControlReportForm extends Form
 
         $columns = array(
             __('plugins.reports.reviewersControlReport.field.fullName'),
+            __('plugins.reports.reviewersControlReport.field.email'),
+            __('plugins.reports.reviewersControlReport.field.affiliation'),
+            __('plugins.reports.reviewersControlReport.field.interests'),
+            __('plugins.reports.reviewersControlReport.field.qualityAverage'),
+            __('plugins.reports.reviewersControlReport.field.reviewedSubmissionsTotal'),
         );
 
         $fp = fopen('php://output', 'wt');
         fputcsv($fp, $columns);
+
+        $reviewersDao = new ReviewersControlReportDAO();
 
         foreach ($reviewersId as $id) {
             $reviewersData = [];
@@ -23,6 +30,13 @@ class ReviewersControlReportForm extends Form
             $user = $userDao->getById($id);
 
             $reviewersData[] = $user->getLocalizedGivenName() . $user->getLocalizedFamilyName();
+            $reviewersData[] = $user->getEmail();
+            $reviewersData[] = $user->getLocalizedAffiliation();
+            $reviewersData[] = $user->getInterestString();
+            $rating = $reviewersDao->getQualityAverage($user->getId());
+            $completedSubmissions = $reviewersDao->getTotalReviewedSubmissions($user->getId());
+            $reviewersData[] = $rating > 0 ? $rating : "";
+            $reviewersData[] = $completedSubmissions > 0 ? $completedSubmissions : "";
 
             fputcsv($fp, $reviewersData);
         }
